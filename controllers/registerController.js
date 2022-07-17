ce46677c83193319077eb294e22214a6e387cda0
jabbar-dev <rashdijabbar@gmail.com>
2022-07-17 19:23:55 +0500

    Adds Register Controller

---
+++ controllers/registerController.js
@@ -0,0 +1,39 @@
+const usersDB = {
+    users : require('../model/users.json'),
+    setUsers : function (data) {this.users = data}
+}
+
+const fsPromises  = require('fs').promises;
+const path = require('path');
+
+//Install BCrypt to Hash and Save Passwords
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async(req, res) =>{
+    const {user, password} = req.body
+    if(!user || !password) return res.sendStatus(400).json({'message':'Missing user or password'});
+
+    //Check for Duplicate Username In DB
+    const duplicate = usersDB.users.find(person => person.username === user);
+    if(duplicate) return res.sendStatus(409); //Conflict
+    
+    try{
+        //Encrypt Password
+        const hashedPWD = await bcrypt.hash(password, 10); 
+
+        //Store the new user in the DB
+        const newUser = {"username" : user, "password":hashedPWD}
+        usersDB.setUsers([...usersDB.users, newUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '../model/users.json'),
+            JSON.stringify(usersDB.users)
+        )
+        console.log(usersDB.users);
+        res.status(201).json({'success': `new User ${user} created`});
+
+    }catch(err){
+        res.sendStatus(500).json({'message':'Error Creating User'});
+    }
+}
+
+module.exports = {handleNewUser};
